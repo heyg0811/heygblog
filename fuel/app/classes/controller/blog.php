b9edbcac1f078731a4f6365730592b1700ce5563
HeyG <dvlpyeti@HeyG.local>
2014-07-15 03:41:28 +0900

    fix the blog tab page

--- fuel/app/classes/controller/blog.php
+++ fuel/app/classes/controller/blog.php
@@ -31,38 +31,18 @@ class Controller_Blog extends Controller_Template
         $this->template->title = 'ぶろぐ';
         $this->template->content = View::forge('blog/index');
         $this->template->breadcrumb = array(array("url" => "/blog/", "name" => "Blog"));
-        $workArt = Model_Article::getCate("work");
-        $realArt = Model_Article::getCate("real");
-        $gameArt = Model_Article::getCate("game");
-
-        if(!empty($_GET['id'])){
-                $temp = Model_Article::getId(Input::get("id",false));
-                $this->template->content->category = $temp[0]["category"];
-                switch($temp[0]["category"]){
-                        case "work": $workArt = Model_Article::getId(Input::get("id",false));break;
-                        case "real": $realArt = Model_Article::getId(Input::get("id",false));break;
-                        case "game": $gameArt = Model_Article::getId(Input::get("id",false));break;
-                }
-                Model_Counter::insertAddress("blog",Input::get("id",false));
+        $id = Input::get("id",null);
+        if(!empty($id)){
+            $article = Model_Article::getId($id);
+            $comment = Model_Comment::getArtId($id);
+            Model_Counter::insertAddress("blog",$id);
         }else{
-                $this->template->content->category = "work";
-                Model_Counter::insertAddress("blog");
+            $article = Model_Article::getAll(1);
+            $comment = Model_Comment::getArtId($article[0]["article_id"]);
+            Model_Counter::insertAddress("blog");
         }
-        $newArt = array(
-                "work" => array(
-                    "article" => $workArt,
-                    "comment" => Model_Comment::getArtId($workArt[0]["article_id"]),
-                    ),
-                "real" => array(
-                    "article" => $realArt,
-                    "comment" => Model_Comment::getArtId($realArt[0]["article_id"]),
-                    ),
-                "game" => array(
-                    "article" => $gameArt,
-                    "comment" => Model_Comment::getArtId($gameArt[0]["article_id"]),
-                    ),
-                );
-        $this->template->content->newArt = $newArt;
+        $this->template->content->article = $article;
+        $this->template->content->comment = $comment;
     }
 
     public function action_confirm(){
--- fuel/app/views/blog/index.php
+++ fuel/app/views/blog/index.php
@@ -1,57 +1,33 @@
 <div class="col-md-8 blog-main">
-	<div class="tabbable">
-		<ul class="nav nav-tabs">
-			<?php foreach($newArt as $val):?>
-			<?php if($val["article"][0]["category"] == $category):?>
-			<li class="active">
-				<a href=<?php echo "#".$val["article"][0]["category"];?> data-toggle="tab" class="tab">
-					<?php echo $val["article"][0]["category"];?>
-				</a>
-			</li>
-		<?php else:?>
-		<li class="">
-			<a href=<?php echo "#".$val["article"][0]["category"];?> data-toggle="tab" class="tab">
-				<?php echo $val["article"][0]["category"];?>
-			</a>
-		</li>
-	<?php endif;?>
-<?php endforeach;?>
-</ul>
-<div id="my-tab-content" class="tab-content">
-	<?php foreach($newArt as $val):?>
-	<?php if($val["article"][0]["category"] == $category):?>
-	<div class="tab-pane active" id=<?php echo $val["article"][0]["category"];?>>
-	<?php else:?>
-	<div class="tab-pane" id=<?php echo $val["article"][0]["category"];?>>
-	<?php endif;?>
+	<?php foreach($article as $val):?>
 	<form action="/blog/confirm" method="post" accept-charset="utf-8">
-		<input type="hidden" name="id" value=<?php echo $val["article"][0]["article_id"];?>>
+		<input type="hidden" name="id" value=<?php echo $val["article_id"];?>>
 		<article class="blog-post">
 			<header>
-				<h1><?php echo $val["article"][0]["title"]?></h1>
+				<h1><?php echo $val["title"]?></h1>
 				<div class="lead-image">
-					<?php echo Asset::img('blog/'.$val["article"][0]["img"].".gif",array('class'=>'img-responsive'));?>
+					<?php echo Asset::img('blog/'.$val["img"].".gif",array('class'=>'img-responsive'));?>
 					<div class="meta clearfix">
 						<div class="author">
-							<i class="icon-user"></i> <span class="data"><?php echo $val["article"][0]["name"]?></span>
+							<i class="icon-user"></i> <span class="data"><?php echo $val["name"]?></span>
 						</div>
 						<div class="date">
 							<i class="icon-calendar"></i> <span class="data">
-							<?php  echo date("d F Y",$val["article"][0]["created_at"])?>
+							<?php  echo date("d F Y",$val["created_at"])?>
 						</span>
 					</div>
 					<div class="comments">
 						<i class="icon-comments"></i> <span class="data"><a
-						href="#comments"><?php echo count($val["comment"]);?> こめんと</a></span>
+						href="#comments"><?php echo count($comment);?> こめんと</a></span>
 					</div>
 				</div>
 			</div>
 		</header>
 		<div class="body">
-			<?php if(strpos($val["article"][0]["body"],"@") == false):?>
-				<?php echo nl2br($val["article"][0]["body"]);?>
+			<?php if(strpos($val["body"],"@") == false):?>
+				<?php echo nl2br($val["body"]);?>
 			<?php else:?>
-				<?php $temp = explode("@",$val["article"][0]["body"]);?>
+				<?php $temp = explode("@",$val["body"]);?>
 				<?php for($i=0;$i<count($temp);$i++):?>
 					<?php if($i % 2 == 0):?>
 						<?php echo nl2br($temp[$i]);?>
@@ -64,12 +40,12 @@
 	</article>
 
 	<aside class="social-icons clearfix">
-		<a href=<?php echo "http://twitter.com/intent/tweet?text=".Uri::base()."blog?id=".$val["article"][0]["article_id"];?> class="social-icon color-one" onClick="window.open(encodeURI(decodeURI(this.href)),'tweetwindow','width=550, height=450, personalbar=0, toolbar=0, scrollbars=1, resizable=!'); return false;">
+		<a href=<?php echo "http://twitter.com/intent/tweet?text=".Uri::base()."blog?id=".$val["article_id"];?> class="social-icon color-one" onClick="window.open(encodeURI(decodeURI(this.href)),'tweetwindow','width=550, height=450, personalbar=0, toolbar=0, scrollbars=1, resizable=!'); return false;">
 			<div class="inner-circle"></div> <i class="icon-twitter"></i>
 		</a> <a href="" onclick="window.open('https://plus.google.com/share?url='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(document.title),null,'width=550px,height=450, personalbar=0, toolbar=0, scrollbars=1, resizable=!');" class="social-icon color-two">
 		<div class="inner-circle"></div> <i class="icon-google-plus"></i>
 	</a>
-	<a href="" onclick=<?php echo "window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent('".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]."')+'&t='+encodeURIComponent('".$val["article"][0]["title"]."'),null,'width=550px,height=350px');return false;"?> class="social-icon color-three">
+	<a href="" onclick=<?php echo "window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent('".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]."')+'&t='+encodeURIComponent('".$val["title"]."'),null,'width=550px,height=350px');return false;"?> class="social-icon color-three">
 	<div class="inner-circle"></div> <i class="icon-facebook"></i>
 </a>
 </aside>
@@ -78,9 +54,9 @@
 	<hr>
 
 	<h2>
-		<i class="icon-comments"></i> <?php echo count($val["comment"]);?> こめんと
+		<i class="icon-comments"></i> <?php echo count($comment);?> こめんと
 	</h2>
-	<?php foreach($val["comment"] as $val):?>
+	<?php foreach($comment as $val):?>
 	<?php if($val["admin"] == 0):?>
 	<article class='comment'>
 		<header class='clearfix'>
@@ -130,21 +106,5 @@
 	</div>
 </aside>
 </form>
-</div>
 <?php endforeach;?>
-
-<div class="tab-pane" id="Real">
-	<div id="cd_talbe">
-		<p>Real</p>
-	</div>
-</div>
-<div class="tab-pane" id="Game">
-	<div id="live_talbe">
-		<p>Game</p>
-	</div>
-</div>
-</div>
-</div>
-
-
 </div>
