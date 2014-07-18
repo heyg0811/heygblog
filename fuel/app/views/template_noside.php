7d69872a115eebbe0c7d4f2af43b527e1625ccc4
HeyG <dvlpyeti@HeyG.local>
2014-07-18 18:59:19 +0900

    fix the previous

--- fuel/app/views/template_noside.php
+++ fuel/app/views/template_noside.php
@@ -94,7 +94,7 @@
         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <!-- no-side1 -->
         <ins class="adsbygoogle"
-        style="display:inline-block;width:100%;height:250px"
+        style="display:inline-block;width:90%;height:250px"
         data-ad-client="ca-pub-8972128233832989"
         data-ad-slot="7757587752"></ins>
         <script>
@@ -105,7 +105,7 @@
         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <!-- no-side2 -->
         <ins class="adsbygoogle"
-        style="display:inline-block;width:100%;height:250px"
+        style="display:inline-block;width:90%;height:250px"
         data-ad-client="ca-pub-8972128233832989"
         data-ad-slot="9234320950"></ins>
         <script>
@@ -116,7 +116,7 @@
         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <!-- no-side3 -->
         <ins class="adsbygoogle"
-        style="display:inline-block;width:100%;height:250px"
+        style="display:inline-block;width:90%;height:250px"
         data-ad-client="ca-pub-8972128233832989"
         data-ad-slot="1711054151"></ins>
         <script>
