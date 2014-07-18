736711db756535bc16e0067e395a1ea762a65b93
HeyG <dvlpyeti@HeyG.local>
2014-07-18 19:25:49 +0900

    fix the previous

--- fuel/app/views/template_noside.php
+++ fuel/app/views/template_noside.php
@@ -90,38 +90,37 @@
     </div>
 </header>
 <?php echo $content;?>
-<div class="row">
-    <div class="col-md-4">
-
-        <!-- no-side1 -->
-        <ins class="adsbygoogle"
-        style="display:inline-block;width:100%;height:250px"
-        data-ad-client="ca-pub-8972128233832989"
-        data-ad-slot="7757587752"></ins>
-        <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-    </div>
-    <div class="col-md-4">
-        <!-- no-side2 -->
-        <ins class="adsbygoogle"
-        style="display:inline-block;width:100%;height:250px"
-        data-ad-client="ca-pub-8972128233832989"
-        data-ad-slot="9234320950"></ins>
-        <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-    </div>
-    <div class="col-md-4">
-        <!-- no-side3 -->
-        <ins class="adsbygoogle"
-        style="display:inline-block;width:100%;height:250px"
-        data-ad-client="ca-pub-8972128233832989"
-        data-ad-slot="1711054151"></ins>
-        <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-    </div>
+<div class="row" style="margin-right:-30px;margin-left:-30px;">
+        <div class="col-md-4">
+            <!-- no-side1 -->
+            <ins class="adsbygoogle"
+            style="display:inline-block;width:100%;height:250px"
+            data-ad-client="ca-pub-8972128233832989"
+            data-ad-slot="7757587752"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
+        <div class="col-md-4">
+            <!-- no-side2 -->
+            <ins class="adsbygoogle"
+            style="display:inline-block;width:100%;height:250px"
+            data-ad-client="ca-pub-8972128233832989"
+            data-ad-slot="9234320950"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
+        <div class="col-md-4">
+            <!-- no-side3 -->
+            <ins class="adsbygoogle"
+            style="display:inline-block;width:100%;height:250px"
+            data-ad-client="ca-pub-8972128233832989"
+            data-ad-slot="1711054151"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
 </div>
 <footer>
     <div class="widewrapper footer">
