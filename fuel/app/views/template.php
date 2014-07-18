539cb3d555b1816a8121b5865038cc99752cacd5
HeyG <dvlpyeti@HeyG.local>
2014-07-18 19:31:12 +0900

    fix the previous

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -149,10 +149,10 @@
     </ul>
 </div>
 </div>
-<div class="aside-widget">
+<div class="aside-widget margin-left1">
     <!-- top -->
     <ins class="adsbygoogle"
-     style="margin-left:10px;margin-right:-10pxdisplay:inline-block;width:100%;height:250px"
+     style="display:inline-block;width:300px;height:250px"
      data-ad-client="ca-pub-8972128233832989"
      data-ad-slot="8815518551"></ins>
 </div>
